784368ba0c7102defcfd6aad26dcd8d5673e16cc
veyselguven <vgbasbaydar@gmail.com>
2023-08-26 19:00:29 +0300

    diving to bug of stack navigator

--- App.js
+++ App.js
@@ -6,3 +6,25 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createStackNavigator,
+} from "react-navigation";
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainflow: createMaterialBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen,
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
+});
+
+export default createAppContainer(switchNavigator);
