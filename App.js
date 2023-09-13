3abdda941ff36e0097aed37a051d90aa160a0d10
veyselguven <vgbasbaydar@gmail.com>
2023-09-13 19:38:41 +0300

    fixed the bug of navigation due to new version

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from "react";
-
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
-import {
-  createAppContainer,
-  createSwitchNavigator,
-  createStackNavigator,
-} from "react-navigation";
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen,
   }),
-  mainflow: createMaterialBottomTabNavigator({
+  mainFlow: createMaterialBottomTabNavigator({
     trackListFlow: createStackNavigator({
       TrackList: TrackListScreen,
       TrackDetail: TrackDetailScreen,
@@ -28,3 +25,35 @@ const switchNavigator = createSwitchNavigator({
 });
 
 export default createAppContainer(switchNavigator);
+
+// import React from "react";
+
+// import AccountScreen from "./src/screens/AccountScreen";
+// import SigninScreen from "./src/screens/SigninScreen";
+// import SignupScreen from "./src/screens/SignupScreen";
+// import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+// import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+// import TrackListScreen from "./src/screens/TrackListScreen";
+// import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+// import {
+//   createAppContainer,
+//   createSwitchNavigator,
+//   createStackNavigator,
+// } from "react-navigation-stack";
+
+// const switchNavigator = createSwitchNavigator({
+//   loginFlow: createStackNavigator({
+//     Signup: SignupScreen,
+//     Signin: SigninScreen,
+//   }),
+//   mainflow: createMaterialBottomTabNavigator({
+//     trackListFlow: createStackNavigator({
+//       TrackList: TrackListScreen,
+//       TrackDetail: TrackDetailScreen,
+//     }),
+//     TrackCreate: TrackCreateScreen,
+//     Account: AccountScreen,
+//   }),
+// });
+
+// export default createAppContainer(switchNavigator);
