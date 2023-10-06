88b5bffae980b5501ca1d2127a641bc6258d08d9
veyselguven <vgbasbaydar@gmail.com>
2023-10-05 20:46:57 -0700

    added react-native-element and create singup screen with

---
+++ src/components/Spacer.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const Spacer = ({ children }) => {
+  return <View style={styles.spacer}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  spacer: { margin: 15 },
+});
+
+export default Spacer;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,22 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+
+      {/* <Text style={{ fontSize: 48 }}>SignupScreen</Text>
       <Button
         title="go to Sign in"
         onPress={() => navigation.navigate("Signin")}
@@ -12,7 +24,7 @@ const SignupScreen = ({ navigation }) => {
       <Button
         title="go to Main flow "
         onPress={() => navigation.navigate("mainFlow")}
-      />
+      /> */}
     </>
   );
 };
