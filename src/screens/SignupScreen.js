163cc1cb6966283b5a7946595c59b089bc1d5c6d
veyselguven <vgbasbaydar@gmail.com>
2023-10-06 20:17:17 -0700

    added state for email and password

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,30 +1,37 @@
 import { View, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        label="Email"
+        value={email}
+        // onChangeText={(newEmail) => setEmail(newEmail)}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
-
-      {/* <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button
-        title="go to Sign in"
-        onPress={() => navigation.navigate("Signin")}
-      />
-      <Button
-        title="go to Main flow "
-        onPress={() => navigation.navigate("mainFlow")}
-      /> */}
     </View>
   );
 };
