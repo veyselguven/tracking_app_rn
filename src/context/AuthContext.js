8d40093b687a5d6dc095fcbe3412cb969884080a
veyselguven <vgbasbaydar@gmail.com>
2023-10-09 23:56:14 -0700

    created actions for singin,signout,singup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,9 +6,29 @@ const authReducer = (state, action) => {
       return state;
   }
 };
+const sigup = () => {
+  return ({ email, password }) => {
+    //make api request to sign up with that email and password
+    // if we sign up,modify our state,and say that we are authenticated
+    // if signing up fails, we probably need to reflect an error somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // try to sigin
+    // handle success by updating state
+    //handle failure by showing error message (somehow)
+  };
+};
+const singout = () => {
+  return () => {
+    // somehow sign out !
+  };
+};
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, singout, signin },
   { isSignedIn: false }
 );
