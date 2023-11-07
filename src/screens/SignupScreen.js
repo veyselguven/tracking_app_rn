ff4e3254ad83e3e22c87fe1fd6e20a89e6363338
veyselguven <vgbasbaydar@gmail.com>
2023-11-06 16:48:49 -0800

    refactor signup screen , created to reusable component for sing in and singup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,42 +1,23 @@
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import React, { useState, useContext } from "react";
-import { Text, Input, Button } from "react-native-elements";
+import React, { useContext } from "react";
+import { Text } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
   // console.log(state);
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        // onChangeText={(newEmail) => setEmail(newEmail)}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sing Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sing Up"
+        // onSubmit={({email,password})=>signup({email,password})}
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        secureTextEntry
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage} </Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
       <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
         <Spacer>
           <Text style={styles.link}>
