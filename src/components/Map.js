05d499b8e940dba8d72f02b7a84da2ddc13b99fc
veyselguven <vgbasbaydar@gmail.com>
2023-11-11 20:22:37 -0800

    working on pollyline component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,23 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03121 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 + i * 0.001,
+      });
+    }
+  }
+
   return (
     <View>
       <Text>I am a Map</Text>
@@ -14,7 +29,9 @@ const Map = () => {
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         }}
-      />
+      >
+        <Polyline coordinates={points} />
+      </MapView>
     </View>
   );
 };
