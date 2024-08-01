c507cc5b00b879023627fe69013d60a30242e26e
veyselguven <vgbasbaydar@gmail.com>
2024-07-31 20:40:37 -0700

    clearing function

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -4,5 +4,5 @@ export default axios.create({
   headers: {
     "ngrok-skip-browser-warning": "true",
   },
-  baseURL: "https://67ab-2601-646-c000-1af0-f4d1-8ca0-8905-c2d7.ngrok-free.app",
+  baseURL: "https://e3e8-2601-640-8c01-1670-645f-c735-a6d-34c.ngrok-free.app",
 });
