a21b5be8ed65888624e7eeeb5f63a13e6722a7d3
veyselguven <vgbasbaydar@gmail.com>
2024-12-09 12:18:36 -0800

    auto authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,8 +1,32 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   headers: {
     "ngrok-skip-browser-warning": "true",
   },
-  baseURL: "https://849c-2601-640-8c81-5fb0-1c4f-6bee-1b14-1f3a.ngrok-free.app",
+  baseURL: "https://73df-2601-640-8c81-5fb0-f497-4414-4697-37be.ngrok-free.app",
 });
+// export default axios.create({
+//   headers: {
+//     "ngrok-skip-browser-warning": "true",
+//   },
+//   baseURL: "https://73df-2601-640-8c81-5fb0-f497-4414-4697-37be.ngrok-free.app",
+// });
+
+// ngrok http 3000
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
